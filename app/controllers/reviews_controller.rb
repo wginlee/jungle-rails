62110450defa0428befa6917eb177838583311dc
Graham Lee <wgin.lee@gmail.com>
2016-12-10 04:19:00 +0000

    implemented posting reviews and averaging reviews per product

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,20 @@
 class ReviewsController < ApplicationController
+
+  before_action :authorize
+
   def create
     product = Product.find(params[:product_id])
     review = product.reviews.new(review_params)
     review.user = current_user
     review.save!
     redirect_to product
+  end
+
+  def destroy
+    review = Review.find params[:id]
+    product = review.product
+    review.destroy
+    redirect_to product, notice: 'Review deleted!'
   end
 
   private
