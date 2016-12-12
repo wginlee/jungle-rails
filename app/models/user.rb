cebb7e789f074c873ce0b05ac033fc8447df3f00
Graham Lee <wgin.lee@gmail.com>
2016-12-12 23:18:10 +0000

    force users to have unique emails and require them to fill in first and last names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
 end
