78954e6d2242011c7adf4d418af6ccfe635b7d51
Graham Lee <wgin.lee@gmail.com>
2016-12-13 00:22:39 +0000

    finished product specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
 
-    let(:product) { Product.new(name: 'Priodut', price: 99, quantity: 0, category: 'Apparel')}
+    cat = Category.new(name: 'Oppai')
+    let(:product) { Product.new(name: 'Priodut', price_cents: 99, quantity: 0, category: cat)}
 
     it 'should have a valid test subject' do
       expect(product).to be_valid
@@ -13,11 +14,33 @@ RSpec.describe Product, type: :model do
     it 'should be invalid without a name' do
       product.name = nil
       expect(product).to_not be_valid
-      prodcut.name = ''
+      product.name = ''
       expect(product).to_not be_valid
       product.name = ' '
       expect(product).to_not be_valid
 
     end
+
+    it 'should be invalid without a price' do
+      product.price_cents = nil
+      expect(product).to_not be_valid
+
+    end
+
+    it 'should be invalid without a quantity' do
+      product.quantity = nil
+      expect(product).to_not be_valid
+
+    end
+
+    it 'should be invalid without a category' do
+      product.category = nil
+      expect(product).to_not be_valid
+
+    end
+
+
+  end
+
 
 end
